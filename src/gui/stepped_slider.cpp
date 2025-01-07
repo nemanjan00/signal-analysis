079a2db62a4e5fcef423bed900c6b14d7048de2d
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2025-01-07 21:25:59 +0100

    License

--- src/gui/stepped_slider.cpp
+++ src/gui/stepped_slider.cpp
@@ -6,7 +6,7 @@
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation, either version 1 of the License, or
  * (at your option) any later version.
  *
  * You should have received a copy of the GNU General Public License
--- src/gui/stepped_slider.h
+++ src/gui/stepped_slider.h
@@ -1,3 +1,18 @@
+/*
+ * This file incorporates code from SDRPlusPlus, originally developed by ryzerth.
+ * SDRPlusPlus is licensed under the GNU General Public License (GPL) version 3.
+ *
+ * Copyright (C) 2025 ryzerth
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 1 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 namespace ImGui {
