9165cb821880f6a1562483e86e03e9502f2416ef
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2025-01-07 17:18:58 +0100

    Switch to imgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,3 @@
-#include <QApplication>
-#include "MainWindow.h"
-
 int main(int argc, char *argv[]) {
-	QApplication app(argc, argv);
-
-	MainWindow w;
-	w.show();
-
-	return app.exec();
+	return 0;
 }
